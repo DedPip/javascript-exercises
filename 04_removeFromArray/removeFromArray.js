ce4017cf89859f1290119c82dc0f469f5f418bb3
DedPip <125252491+DedPip@users.noreply.github.com>
2024-08-29 20:00:43 +0330

    Complete assignment 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...theArgs) {
+  let subtracted = [];
+  for (i = 0; i < array.length; i++)
+    if (!theArgs.includes(array[i])) {
+      subtracted.push(array[i]);
+    }
+    return subtracted;
 };
 
 // Do not edit below this line
