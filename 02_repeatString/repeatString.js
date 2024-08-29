7ccf9440b9e7af5ff8f60716c3c5aff5b00784e1
DedPip <125252491+DedPip@users.noreply.github.com>
2024-08-29 11:15:27 +0330

    Complete first 3 assignments

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  stringSum = '';
+  if (num < 0) {
+    return "ERROR";
+  }
+  else if (string === '') {
+    return '';
+  }
+  else {
+    for (i = 0; i < num; i++) {
+      stringSum += string;
+    }
+    return stringSum
+  }
 };
 
 // Do not edit below this line
