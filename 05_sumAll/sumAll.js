ee6b95e16d79b1e4fd992ad8ecf832c0cd69b738
DedPip <125252491+DedPip@users.noreply.github.com>
2024-08-30 11:20:26 +0330

    Complete assignment 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+  let sum = 0;
+  let bigger = 0;
+  let smaller = 0;
+  if (typeof a === 'number' && typeof b === 'number' && a > 0 && b > 0 && Number.isInteger(a) && Number.isInteger(b)) {
+    if (a > b) {bigger = a; smaller = b}
+    else if (a < b) {bigger = b; smaller = a;}
+    else {bigger = a; smaller = b;}
+    for (i = smaller; i <= bigger; i++) {
+      sum += i;
+    }
+    return sum;
+  } else {return 'ERROR'}
 };
 
 // Do not edit below this line
