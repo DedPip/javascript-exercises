79abbc7a00edf036f55c01da37acea23a1d54e88
DedPip <125252491+DedPip@users.noreply.github.com>
2024-08-30 13:15:44 +0330

    Complete assignment 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+const roundTo1Decimal = function(num) {
+  const rounded = num.toFixed(1);
+  return Number(rounded);
+}
+
+const convertToCelsius = function(tempInF) {
+  const converted = (tempInF - 32) * (5/9)
+  if (Number.isInteger(converted)) return converted
+  else return roundTo1Decimal(converted);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInC) {
+  const converted = (tempInC * (9/5)) + 32;
+  if (Number.isInteger(converted)) return converted
+  else return roundTo1Decimal(converted);
 };
 
 // Do not edit below this line
